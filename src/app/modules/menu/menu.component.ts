698f6bdf8a7af5c2278063205bd10dc1f011be13
Luis Guzman <luisvg18@gmail.com>
2024-07-25 18:16:31 -0600

    Cambios reglas-negocio

--- src/app/modules/menu/menu.component.ts
+++ src/app/modules/menu/menu.component.ts
@@ -15,22 +15,22 @@ export class MenuComponent implements OnInit, OnDestroy {
   showMenu: boolean = false;
   items: MenuItem[] | undefined;
 
-  
+
   constructor(
     private readonly layoutService: LayoutService,
     private router: Router
   ) {
     router.events.subscribe((val) => {
-      // see also 
-      console.log(val instanceof NavigationEnd) 
+      // see also
+      console.log(val instanceof NavigationEnd)
       this.showMenu = false;
   });
   }
 
   ngOnInit() {
-       this.subscription = 
+       this.subscription =
       this.layoutService.showMenu$.subscribe(showMenu => {
-   
+
       this.showMenu = showMenu;
     });
       this.items = [
@@ -48,7 +48,7 @@ export class MenuComponent implements OnInit, OnDestroy {
               {
                 label: 'Reglas de Negocio',
                 icon: 'pi pi-cog',
-                routerLink: ['/construccion']
+                routerLink: ['/reglas-negocio']
             }
             ]
         },
@@ -75,7 +75,7 @@ export class MenuComponent implements OnInit, OnDestroy {
                   }
               ]
           },
-          
+
           {
               separator: true
           }
